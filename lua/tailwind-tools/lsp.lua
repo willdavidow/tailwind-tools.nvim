46e06fb2650644f9fc2ae32a4baa6af0de85c92a
Will Davidow <w.davidow@gmail.com>
2024-11-16 13:01:19 -0600

    chore: trying things

--- lua/tailwind-tools/lsp.lua
+++ lua/tailwind-tools/lsp.lua
@@ -253,7 +253,7 @@ M.show_selection = function(sync)
     class_text[#class_text + 1] = table.concat(text, "\n")
   end
 
-  print(class_text)
+  print(dump(class_text))
 
   -- expand_utilities({ class_ranges })
 end
